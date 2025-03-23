eebc51d78fea07dabc2dba4a6f0ec6195633784b
Jas-5808 <jasursadicov2@gmail.com>
2025-03-23 13:57:07 +0500

    delete comment

--- server/src/app.js
+++ server/src/app.js
@@ -4,24 +4,20 @@ const clickRoutes = require('./routes/clickRoutes');
 const callRoutes = require('./routes/callRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const { port } = require('./utils/config');
-require('./bot/bot'); // Инициализация бота
+require('./bot/bot'); 
 
 const app = express();
 
 app.use(express.json());
 
-// Инициализация базы данных
 (async () => {
     await initDb();
 
-    // Маршруты
     app.use('/api/click', clickRoutes);
     app.use('/api/call', callRoutes);
 
-    // Обработка ошибок
     app.use(errorHandler);
 
-    // Запуск сервера
     app.listen(port, () => {
         console.log(`Сервер запущен на порту ${port}`);
     });
